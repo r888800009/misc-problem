5fd58491c953da84f41f84b8c378d7baa67cdc47
r888800009 <r888800009@gmail.com>
2021-08-20 01:00:48 +0800

    Update md5-hasher

--- md5-hasher/main.py
+++ md5-hasher/main.py
@@ -23,7 +23,7 @@ regex = r'[a-zA-Z/:0-9.]{1,2}'
 
 print('''The message was divided into multiple parts,
 each of which is two bytes long,
-and then every two bytes (or one) was hashed by md5.\n''')
+and then every two bytes (or one) has been hashed by md5.\n''')
 
 print('for example, "aa" is')
 print(hashlib.md5(b'aa').hexdigest())
