b8d7eaebe959e01a1a4772ae4c4d9a8487f92e08
r888800009 <r888800009@gmail.com>
2021-08-23 01:30:41 +0800

    Update hasher to three bytes

--- md5-hasher/main.py
+++ md5-hasher/main.py
@@ -7,42 +7,43 @@ from itertools import product
 
 print('gen validator')  
 print(len(string.printable))
-prod = product(string.printable, repeat=2)
+prod2 = product(string.printable, repeat=2)
+prod3 = product(string.printable, repeat=3)
 mapping = {}
 count = 0
-for i in list(prod) + list(string.printable):
-    two_bytes = ''.join(i)
-    mapping[str(hashlib.md5(bytes(two_bytes, 'ascii')).hexdigest())] = two_bytes
+for i in list(prod2) + list(string.printable) + list(prod3):
+    three_bytes = ''.join(i)
+    mapping[str(hashlib.md5(bytes(three_bytes, 'ascii')).hexdigest())] = three_bytes
     count += 1
 print(count)
 
 question = input('> ')
 
 # check question verified
-regex = r'[a-zA-Z/:0-9.]{1,2}'
+regex = r'[a-zA-Z/:0-9.]{1,3}'
 
 print('''The message was divided into multiple parts,
-each of which is two bytes long,
-and then every two bytes (or one) has been hashed by md5.\n''')
+each of which is three bytes (or one or two) long,
+and then every three bytes (or one or two) has been hashed by md5.\n''')
 
 print('for example, "aa" is')
 print(hashlib.md5(b'aa').hexdigest())
 print('')
 
-print(f'Alphabet list {regex}\n')
+print(f'each part matches this regex {regex}\n')
 
 print('Message md5:')
-for a in range(0,len(question), 2):
-    two_bytes = question[a+0:a+2]
-    assert re.fullmatch(regex, two_bytes), 'valid question'
-    #print(two_bytes)
-    print(hashlib.md5(bytes(two_bytes, 'ascii')).hexdigest())
+for a in range(0,len(question), 3):
+    three_bytes = question[a+0:a+3]
+    #print(three_bytes)
+    assert re.fullmatch(regex, three_bytes), 'valid question'
+    print(hashlib.md5(bytes(three_bytes, 'ascii')).hexdigest())
 
 print('\n\n\n')
 print('validating')
-for a in range(0,len(question), 2):
-    two_bytes = question[a+0:a+2]
-    hash1 = hashlib.md5(bytes(two_bytes, 'ascii')).hexdigest() 
+for a in range(0,len(question), 3):
+    three_bytes = question[a+0:a+3]
+    hash1 = hashlib.md5(bytes(three_bytes, 'ascii')).hexdigest() 
     print(mapping[str(hash1)])
 
 
